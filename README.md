6249bcc37457f14b7eb77c86f9575eeb9403b011
Tongze Wang <wtongze@gmail.com>
2023-01-29 12:35:57 -0800

    feat: add README

---
+++ README.md
@@ -0,0 +1,8 @@
+# CarRentalKit - Android
+
+> This is a demo Android app for CarRentalKit.
+
+Backend Code can be found here:
+<https://github.com/wtongze/CarRentalKit>
+
+This app is only for demo purposes.
--- app/src/main/java/com/wtongze/carrentalkit/data/QuoteState.kt
+++ app/src/main/java/com/wtongze/carrentalkit/data/QuoteState.kt
@@ -7,8 +7,8 @@ import java.time.LocalTime
 data class QuoteState (
     val location: String = "San Jose, CA",
     val pickUpDate: LocalDate = LocalDate.now(),
-    val pickUpTime: LocalTime = formatTime(LocalTime.now()),
+    val pickUpTime: LocalTime = formatTime(LocalTime.now().plusMinutes(30)),
     val dropOffDate: LocalDate = LocalDate.now().plusDays(1),
-    val dropOffTime: LocalTime = formatTime(LocalTime.now()),
+    val dropOffTime: LocalTime = formatTime(LocalTime.now().plusMinutes(30)),
     val coupon: String = ""
 )
